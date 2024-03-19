0869afa56ed3b1134b2ed90c38a673e8765a28f9
Allam9573 <aallamdaniel@gmail.com>
2024-03-19 01:25:49 -0600

    ajustes

--- compras/models.py
+++ compras/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from proveedores.models import Proveedor
 
 # Create your models here.
+class Compra(models.Model):
+    tipo_compra = models.CharField(max_length=15)
+    nombre = models.CharField(max_length=30)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    proveedor
