8ac219392b5723447c350e43d4f36467cf69a142
Allam9573 <aallamdaniel@gmail.com>
2024-03-31 10:40:54 -0600

    config

--- compras/models.py
+++ compras/models.py
@@ -6,4 +6,3 @@ class Compra(models.Model):
     tipo_compra = models.CharField(max_length=15)
     nombre = models.CharField(max_length=30)
     precio = models.DecimalField(max_digits=10, decimal_places=2)
-    proveedor
